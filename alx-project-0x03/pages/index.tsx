50f6f2295bae96f833dca635693e5d26e4f36115
Victor Muhita <wangombevictor5@gmail.com>
2025-07-27 21:59:53 +0300

    add all

--- alx-project-0x03/pages/index.tsx
+++ alx-project-0x03/pages/index.tsx
@@ -14,6 +14,7 @@ export default function Home() {
     <div className="min-h-screen bg-gray-100 flex flex-col justify-center items-center text-center">
       {/* Welcome Message */}
       <h1 className="text-4xl font-bold text-gray-800 mb-4">
+        {/* <h1> Welcome to Splash App </h1> */}
         Welcome to Splash App!
       </h1>
       <p className="text-lg text-gray-600 mb-8">
